18814f64d4b8820cee0b2e351b9006d16a7ac2d3
KaueYS <97792312+KaueYS@users.noreply.github.com>
2022-11-20 01:11:15 -0300

    Adicionar arquivos de projeto.

---
+++ CSharp-Exercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kaue";
+            int message = 3;
+            double tempC = 34.4;
+            double tempF = 94;
+            double celsius = (tempF - 32) * 5 / 9;
+
+
+
+            Console.WriteLine($"Olá, {name}!, Voce tem {message} mensagens na sua caixa de entrada. A temperatura é de {tempC}");
+
+            Console.WriteLine($"A temperatura é de {celsius.ToString("F2")} celsius");
+        }
+    }
+}
