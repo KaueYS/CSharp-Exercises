9a8012a34fb91128ef3c1a80d0c8a71f37c4f1af
KaueYS <97792312+KaueYS@users.noreply.github.com>
2022-11-26 16:47:43 -0300

    Matrizes

--- CSharp-Exercises/Program.cs
+++ CSharp-Exercises/Program.cs
@@ -58,7 +58,20 @@ namespace CSharp_Exercises
             {
                 Console.WriteLine(" Your subscription will not expire soon!");
             }
-            
+
+            // Matrizes
+
+            string[] fraudIds = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+            foreach (string fraud in fraudIds)
+            {
+                //Console.WriteLine(fraud);
+                if (fraud.StartsWith("G"))
+                {
+                    Console.WriteLine(fraud);
+                }
+            }
+
         }
     }
 }
